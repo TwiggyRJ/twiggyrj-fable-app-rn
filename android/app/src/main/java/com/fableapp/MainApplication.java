0c9f1cfe509f0d12841fe9d1298bfc2b2159725d
Aaron Fryer <aaronfvaio@msn.com>
2018-02-28 21:50:53 +0000

    Added Fabric into Android App

--- android/app/src/main/java/com/fableapp/MainApplication.java
+++ android/app/src/main/java/com/fableapp/MainApplication.java
@@ -1,8 +1,9 @@
 package com.fableapp;
 
 import android.app.Application;
-
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
+import com.smixx.fabric.FabricPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -11,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,10 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new KeychainPackage(),
-            new ReactNativeConfigPackage()
+        new MainReactPackage(),
+        new FabricPackage(),
+        new VectorIconsPackage(),
+        new KeychainPackage(),
+        new ReactNativeConfigPackage()
       );
     }
 
@@ -46,6 +49,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
