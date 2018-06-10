14b8252582c9e9d46200cf2ff6a490ffc89469ec
Aaron Fryer <aaronfvaio@msn.com>
2018-06-10 16:04:35 +0100

    reorganised some routes

--- app/routes/main/bookmarks/bookmarksContainer.js
+++ app/routes/main/bookmarks/bookmarksContainer.js
@@ -1,12 +1,17 @@
 import { createStackNavigator } from 'react-navigation';
 import { headerStyle } from '../../../config/styles';
 import Bookmarks from './bookmarks';
+import Listing from '../listing';
 
 const BookmarksContainer = createStackNavigator(
   {
     Bookmarks: {
       path: '/bookmarks/all',
       screen: Bookmarks,
+    },
+    Listing: {
+      path: '/bookmarks/listing',
+      screen: Listing,
     },
   },
   {
--- app/routes/main/index.js
+++ app/routes/main/index.js
@@ -10,6 +10,7 @@ import Story from './story';
 import Bookmarks from './bookmarks';
 import Listing from './listing';
 import Menu from './menu';
+import Notifications from './notifications';
 
 const TabNav = createBottomTabNavigator(
   {
@@ -44,8 +45,8 @@ const TabNav = createBottomTabNavigator(
       },
     },
     NotificationsTab: {
-      screen: Menu,
-      path: '/settings',
+      screen: Notifications,
+      path: '/notifications',
       navigationOptions: {
         title: 'Notifications',
         tabBarAccessibilityLabel: 'Notifications',
